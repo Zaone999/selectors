87aadd66f43c4d64bb871c545d65db3cbce400ff
Zaone999 <waelbougarfa@gmail.com>
2024-03-02 14:23:17 +0100

    some changes

--- django-site/toDoApp/taskManager/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("handle/<int:task_id>/", views.handle_request, name="handle_request"),
-]
